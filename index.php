196c4dd55e5bcdee39b08eb2eda66c11bc7af6a2
unknown <wmoncada@tecsup.edu.pe>
2024-04-12 20:45:42 -0500

    version actualizada commit

--- index.php
+++ index.php
@@ -53,13 +53,21 @@
 	</div> 
 	<!-- /.navbar -->
 
+	<style>
+		.hola{
+			background-color: blue;
+		}
+ 
+	</style>
+
 	<!-- Header -->
 	<header id="head">
 		<div class="container">
 			<div class="row">
-				<h1 class="lead">WELCOME STUDENTS DDS</h1>
+				<h1 class="lead"><strong>WELCOME STUDENTS DDS</strong></h1>
 				<p class="tagline">Esto es una prueba de commit y nuevo push en terminal <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
-				<p><a class="btn btn-default btn-lg" role="button">MORE INFO</a> <a class="btn btn-action btn-lg" role="button">DOWNLOAD NOW</a></p>
+				<p><a class="btn btn-default  hola btn-lg" role="button">MORE INFO</a> 
+				<a class="btn btn-action btn-lg" role="button">DOWNLOAD NOW</a></p>
 			</div>
 		</div>
 	</header>
